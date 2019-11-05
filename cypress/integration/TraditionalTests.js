27a42b09fffb7f72f27013ec1479e7baf2341675
Daniel Sanchez <daniel.edw.sanchez@gmail.com>
2019-11-05 09:16:49 -0500

    Login Page UI Elements test / page object V1

--- cypress/integration/TraditionalTests.js
+++ cypress/integration/TraditionalTests.js
@@ -6,8 +6,31 @@ describe('Hackathon App V1 tests', () => {
 
     const lpv1 = new HackathonAppV1
 
-    it('Login page UI elements test', () => {
+    beforeEach(() => {
+        lpv1.visitLoginPageV1()
+    })
 
+    it('Login page UI elements test', () => {
+        lpv1.authWrapper().should('be.visible')
+        lpv1.logo().should('be.visible')
+        lpv1.logoArea().should('be.visible')
+        lpv1.loginFormHeader().should('be.visible').should('contain.text', 'Login Form')
+        lpv1.alertEmpty().should('be.visible')
+        lpv1.formArea().should('be.visible')
+        lpv1.usernameLabel().should('be.visible').should('contain.text', 'Username')
+        lpv1.usernameIcon().should('be.visible')
+        lpv1.usernameField().should('be.visible').should('be.enabled')
+        lpv1.passwordLabel().should('be.visible').should('contain.text', 'Password')
+        lpv1.passwordIcon().should('be.visible')
+        lpv1.passwordField().should('be.visible').should('be.enabled')
+        lpv1.loginButton().should('be.visible').should('contain.text', 'Log In')
+        lpv1.rememberMeLabel().should('be.visible').should('contain.text', 'Remember Me')
+        lpv1.rememberMeCheckbox().should('be.visible').should('not.be.checked')
+        lpv1.buttonArea().should('be.visible')
+        lpv1.socialButtonArea().should('be.visible')
+        lpv1.twitterIcon().should('be.visible')
+        lpv1.facebookIcon().should('be.visible')
+        lpv1.linkedInIcon().should('be.visible')
     })
 
     it('Data-driven test', () => {
--- cypress/integration/VisualAITests.js
+++ cypress/integration/VisualAITests.js
@@ -2,6 +2,11 @@
 
 import { HackathonAppV2 } from "../page-objects/hackathon-app-v2.spec"
 
+// You have just been added to the team: Applitools Hackathon 2019 (daniel.sanchez@silverlinecrm.com)
+// You can access the team's dashboard here: https://eyes.applitools.com/?accountId=cB2zgF7jCEaBPGAD3jdcKg~~&userName=daniel.sanchez%40silverlinecrm.com&source=&
+// Your username is: daniel.sanchez@silverlinecrm.com
+// Your API key for running visual tests is: 3NrJB25IVoPS1k85TXUb102qACnCac93iYpjAX8E3E8sw110
+
 describe('Hackathon App V2 tests', () => {
 
     const lpv1 = new HackathonAppV2
